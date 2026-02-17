28231cfbbdd99eb88051bb73e80e6384c2e6a0fa
JulieDandrimont <90048560+JulieDandrimont@users.noreply.github.com>
2026-02-17 11:01:09 -0500

    add read me and prisma db

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express();
 
 app.use(express.json());
@@ -11,6 +14,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Server is running on http://localhost:${port}');
 });

7b33e14c76824a89796e7032a1cd79079dfaccb4
JulieDandrimont <90048560+JulieDandrimont@users.noreply.github.com>
2026-02-18 09:10:17 -0500

    modifies prisma database

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,9 +1,9 @@
 import {createUser, findUserByEmail} from "./users.service.js";
 import {validateUser} from "./users.validation.js";
 
-export async function handleCreateUser(request, response) {             //cette fonction remplace la route post /users
+export async function handleCreateUser(req, response) {             //cette fonction remplace la route post /users
     try {
-        const result = validateUser(request.body);
+        const result = validateUser(req.body);
 
         if (!result.ok) {                                           // Si la validation échoue, on retourne une réponse d'erreur 
             return response.status(400).json({                      // avec les détails des erreurs
